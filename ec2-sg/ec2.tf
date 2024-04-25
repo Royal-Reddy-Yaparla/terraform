c0018da92bffeb2ef2f5af714ea0d99b413b9030
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-25 20:29:29 +0530

    terraform-pactice

---
+++ ec2-sg/ec2.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.practicetf.id]
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
+resource "aws_security_group" "practicetf" {
+  name        = "practice_sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  # vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "practice_sg"
+  }
+
+  ingress {
+    description = "Allow all ports"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "all"
+    cidr_blocks      = var.cidr_blocks
+    # ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    # ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+
